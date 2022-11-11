e240006792bce9afd54e50f0762be83aeed3eaad
denis <106389581+fishman123456@users.noreply.github.com>
2022-11-11 17:10:08 +0700

    добавили цикл
    можно слои выводить 10 000
    100 000 в ошибку уходит

--- Layer_Add/Class1.cs
+++ Layer_Add/Class1.cs
@@ -7,7 +7,7 @@ using Autodesk.AutoCAD.DatabaseServices;
  
 namespace HabrPlug_Layers
 {
-    // Создан 24-12-2021 (спизжен)
+    // Создан 24-12-2021 ()
     //(vl-cmdf "_netload" "c:/myapps/mycommands.dll") - лиспом dll загружаем
     // "C:\Users\Fishman.DB.CORP\YandexDisk\Работа АСКО\AUTOCAD_Плагины\02-07-2021_\Layer_Add\bin\Debug\Layer_Add.dll"
     // (vl-cmdf "_netload" "C:\Users\Fishman.DB.CORP\YandexDisk\Работа АСКО\AUTOCAD_Плагины\02-07-2021_\Layer_Add\bin\Debug\Layer_Add.dll")
@@ -28,25 +28,30 @@ namespace HabrPlug_Layers
             // блокируем документ
             using (DocumentLock docloc = acDoc.LockDocument())
             {
+                for (int i = 0; i < 100000; i++)
+                {
+
+               
                 // начинаем транзакцию
                 using (Transaction tr = acCurDb.TransactionManager.StartTransaction())
                 {
-                  
+
                     // открываем таблицу слоев документа
                     LayerTable acLyrTbl = tr.GetObject(acCurDb.LayerTableId, OpenMode.ForWrite) as LayerTable;
 
                     // создаем новый слой и задаем ему имя
                     LayerTableRecord acLyrTblRec = new LayerTableRecord();
-       
-                    acLyrTblRec.Name = "301.R-K1";
+                        string dop_layer= "105 - "+i.ToString()+ " - S - " + (i/100).ToString();
+                    acLyrTblRec.Name = dop_layer;
                     // заносим созданный слой в таблицу слоев
                     acLyrTbl.Add(acLyrTblRec);
                     // добавляем созданный слой в документ
                     tr.AddNewlyCreatedDBObject(acLyrTblRec, true);
-
                     // фиксируем транзакцию
                     tr.Commit();
                 }
+                }
+
             }
         }
 
