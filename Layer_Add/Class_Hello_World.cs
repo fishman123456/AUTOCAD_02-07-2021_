516bf0867c7eb98fc9733230df9eee1daaba7387
denis <106389581+fishman123456@users.noreply.github.com>
2022-11-16 17:03:04 +0700

    вложил библиотеки
    Чтобы было что почитать, ещё надо
    Autocad2020_dot_net_training.zip
    на нотбук затащить, вообще то она в САПР в корне лежит

---
+++ Layer_Add/Class_Hello_World.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.IO;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+ 
+namespace HabrPlug_Layers
+{
+    // Создан 24-12-2021 ()
+    //(vl-cmdf "_netload" "c:/myapps/mycommands.dll") - лиспом dll загружаем
+    // "C:\Users\Fishman.DB.CORP\YandexDisk\Работа АСКО\AUTOCAD_Плагины\02-07-2021_\Layer_Add\bin\Debug\Layer_Add.dll"
+    // (vl-cmdf "_netload" "C:\Users\Fishman.DB.CORP\YandexDisk\Работа АСКО\AUTOCAD_Плагины\02-07-2021_\Layer_Add\bin\Debug\Layer_Add.dll")
+    //https://habr.com/ru/post/249015/
+    //https://habr.com/ru/post/235723/
+    //
+    public class Commands : IExtensionApplication
+    {
+        // эта функция будет вызываться при выполнении в AutoCAD команды "TestCommand"
+        [CommandMethod("Layer_Add")]
+        public void MyCommand()
+        {
+// получаем текущий документ и его БД
+            Document acDoc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+            /*Устанавливаем русский шрифт*/
+           
+            // блокируем документ
+            using (DocumentLock docloc = acDoc.LockDocument())
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+
+               
+                // начинаем транзакцию
+                using (Transaction tr = acCurDb.TransactionManager.StartTransaction())
+                {
+
+                    // открываем таблицу слоев документа
+                    LayerTable acLyrTbl = tr.GetObject(acCurDb.LayerTableId, OpenMode.ForWrite) as LayerTable;
+
+                    // создаем новый слой и задаем ему имя
+                    LayerTableRecord acLyrTblRec = new LayerTableRecord();
+                        string dop_layer= "105 - "+i.ToString()+ " - S - " + (i/100).ToString();
+                    acLyrTblRec.Name = dop_layer;
+                    // заносим созданный слой в таблицу слоев
+                    acLyrTbl.Add(acLyrTblRec);
+                    // добавляем созданный слой в документ
+                    tr.AddNewlyCreatedDBObject(acLyrTblRec, true);
+                    // фиксируем транзакцию
+                    tr.Commit();
+                }
+                }
+
+            }
+        }
+
+        // Функции Initialize() и Terminate() необходимы, чтобы реализовать интерфейс IExtensionApplication
+        public void Initialize()
+        {
+
+        }
+
+        public void Terminate()
+        {
+
+        }
+    }
+}
